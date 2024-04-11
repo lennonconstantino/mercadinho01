087e009f8111cd1f207fad9ae7d393104ee4c91b
Lennon Constantino <lennonconstantino@gmail.com>
2024-04-10 23:04:20 -0300

    first commit

---
+++ src/main/java/br/com/lennon/mercadinho01/Mercadinho01Application.java
@@ -0,0 +1,13 @@
+package br.com.lennon.mercadinho01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mercadinho01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mercadinho01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lennon/mercadinho01/Mercadinho01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lennon.mercadinho01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mercadinho01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
