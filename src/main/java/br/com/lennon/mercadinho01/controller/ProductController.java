dba82a38d69b700ca0c63d2b8b39b1d36fb35fe2
Lennon Constantino <lennonconstantino@gmail.com>
2024-04-17 21:41:50 -0300

    adicionando controller Product e banco de dados

---
+++ src/main/java/br/com/lennon/mercadinho01/controller/ProductController.java
@@ -0,0 +1,72 @@
+package br.com.lennon.mercadinho01.controller;
+
+import br.com.lennon.mercadinho01.model.Product;
+import br.com.lennon.mercadinho01.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public Iterable<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable long id) {
+        Optional<Product> optProduct = productRepository.findById(id);
+        return optProduct.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product productCreated = productRepository.save(product);
+
+        return new ResponseEntity<Product>(productCreated, HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable("id") long id) {
+        if (productRepository.existsById(id)) {
+            product.setId(id);
+            Product productUpdated = productRepository.save(product);
+
+            return new ResponseEntity<Product>(productUpdated, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") long id) {
+        Optional<Product> optProduct = productRepository.findByCode(id);
+        if (optProduct.isPresent()) {
+            Product product = optProduct.get();
+            productRepository.delete(product);
+
+            return new ResponseEntity<Product>(product, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/bycode")
+    public ResponseEntity<Product> findById(@RequestParam String code) {
+        Optional<Product> optProduct = productRepository.findByCode(code);
+        return optProduct.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
---
+++ src/main/java/br/com/lennon/mercadinho01/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package br.com.lennon.mercadinho01.repository;
+
+import br.com.lennon.mercadinho01.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    Optional<Product> findByCode(String code);
+}
