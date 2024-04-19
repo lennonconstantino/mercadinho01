41f3ef711f1fc976bd648af1f03e3cf6af9cd364
Lennon Constantino <lennonconstantino@gmail.com>
2024-04-18 21:25:12 -0300

    configurando SNS topico

---
+++ src/main/java/br/com/lennon/mercadinho01/config/SnsConfig.java
@@ -0,0 +1,34 @@
+package br.com.lennon.mercadinho01.config;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.Topic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sns.*;
+
+@Configuration
+public class SnsConfig {
+
+    @Value("${aws.config}")
+    private String awsRegion;
+
+    @Value("${aws.config}")
+    private String productEventsTopic;
+
+
+    @Bean
+    public AmazonSNS snsClient() {
+        return AmazonSNSClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+
+    @Bean(name = "productEventsTopic")
+    public Topic snsProductEventsTopic() {
+        return new Topic().withTopicArn(productEventsTopic);
+    }
+}
