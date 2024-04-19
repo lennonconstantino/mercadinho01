7e3a0dab57a8fd19344b61785bc65a887d2664db
Lennon Constantino <lennonconstantino@gmail.com>
2024-04-18 21:34:29 -0300

    criando os modelos para representar os eventos

---
+++ src/main/java/br/com/lennon/mercadinho01/enums/EventType.java
@@ -0,0 +1,7 @@
+package br.com.lennon.mercadinho01.enums;
+
+public enum EventType {
+    PRODUCT_CREATED,
+    PRODUCT_UPDATED,
+    PRODUCT_DELETED
+}
---
+++ src/main/java/br/com/lennon/mercadinho01/model/Envelope.java
@@ -0,0 +1,22 @@
+package br.com.lennon.mercadinho01.model;
+
+public class Envelope {
+    private String eventType;
+    private String data;
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
